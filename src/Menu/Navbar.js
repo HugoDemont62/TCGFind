9f8347e4f6d0e9acf18bf06890963777f0eec2ee
Drique Noah <nwyuxtfm@gmail.com>
2023-01-30 12:14:25 +0100

    modifs upgrades

---
+++ src/Menu/Navbar.js
@@ -0,0 +1,51 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Route, Link, Routes } from "react-router-dom";
+import { useState } from "react";
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBIcon,
+    MDBCollapse
+} from 'mdb-react-ui-kit';
+import Getpok from "../Pokemon/Getpok";
+
+export function Menub() {
+    const [showNavSecond, setShowNavSecond] = useState(false);
+
+    return (
+        <>
+            <MDBNavbar expand='lg' light bgColor='light'>
+                <MDBContainer fluid>
+                    <MDBNavbarBrand href='#'>TCGFind</MDBNavbarBrand>
+                    <MDBNavbarToggler
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                        onClick={() => setShowNavSecond(!showNavSecond)}
+                    >
+                        <MDBIcon icon='bars' fas />
+                    </MDBNavbarToggler>
+                    <MDBCollapse navbar show={showNavSecond}>
+                        <MDBNavbarNav>
+                            <MDBNavbarLink active aria-current='page' href='/'>
+                                Home
+                            </MDBNavbarLink>
+                            <MDBNavbarLink><Link to="/Getpok">Cards</Link></MDBNavbarLink>
+                            <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                                Disabled
+                            </MDBNavbarLink>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
+            <Routes>
+                <Route exact={true} path="/Getpok" element={<Getpok />}/>
+            </Routes>
+        </>
+    );
+}
